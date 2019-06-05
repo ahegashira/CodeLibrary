62545ccedfa4fb25f18c566778a1a3117b1669c1
Hiro Egashira <ahegashira@gmail.com>
2019-06-05 15:38:40 -0700

    add detail views

--- codelibproj/codelibapp/urls.py
+++ codelibproj/codelibapp/urls.py
@@ -5,8 +5,13 @@ urlpatterns=[
     path('', views.index, name='index'),
     path('getresourcetypes/', views.getResourceTypes, name='types'),
     path('getbooks/', views.getBooks, name='books'),
+    path('getBookDetails/<int:id>', views.getBookDetails, name='book_detail'),
     path('getblogs/', views.getBlogs, name='blogs'),
+    path('getBlogDetails/<int:id>', views.getBlogDetails, name='blog_detail'),
     path('getwebsites/', views.getWebsites, name='websites'),
+    path('getWebsiteDetails/<int:id>', views.getWebsiteDetails, name='website_detail'),
     path('getmeetups/', views.getMeetups, name='meetups'),
+    path('getMeetupDetails/<int:id>', views.getMeetupDetails, name='meetup_detail'),
     path('getdevelopers/', views.getDevelopers, name='developers'),
+    path('getDeveloperDetails/<int:id>', views.getDeveloperDetails, name='developer_detail'),
 ]
